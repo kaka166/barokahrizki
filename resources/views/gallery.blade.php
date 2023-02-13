427149f860fe2143b25ac5dd36d285656efc5155
kaka166 <hafizhfaruq858@gmail.com>
2023-02-13 08:35:04 +0700

    Project

---
+++ resources/views/gallery.blade.php
@@ -0,0 +1,165 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="keywords" content="">
+	<meta name="description" content="">
+
+	<title>Barokah Rizki MAotor</title>
+<!--
+Snapshot Template
+http://www.templatemo.com/tm-493-snapshot
+
+Zoom Slider
+https://vegas.jaysalvat.com/
+
+Caption Hover Effects
+http://tympanus.net/codrops/2013/06/18/caption-hover-effects/
+-->
+    <link rel="stylesheet" href="{{asset('master/css/bootstrap.min.css')}}">
+	<link rel="stylesheet" href="{{asset('master/css/animate.min.css')}}">
+	<link rel="stylesheet" href="{{asset('master/css/font-awesome.min.css')}}">
+  	<link rel="stylesheet" href="{{asset('master/css/component.css')}}">
+	
+    <link rel="stylesheet" href="{{asset('master/css/owl.theme.css')}}">
+	<link rel="stylesheet" href="{{asset('master/css/owl.carousel.css')}}">
+	<link rel="stylesheet" href="{{asset('master/css/vegas.min.css')}}">
+	<link rel="stylesheet" href="{{asset('master/css/style.css')}}">
+
+	<!-- Google web font  -->
+	<link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700,300' rel='stylesheet' type='text/css'>
+	
+</head>
+<body id="top" data-spy="scroll" data-offset="50" data-target=".navbar-collapse">
+
+
+<!-- Preloader section -->
+
+<div class="preloader">
+     <div class="sk-spinner sk-spinner-pulse"></div>
+</div>
+
+
+<!-- Navigation section  -->
+
+  <div class="navbar navbar-default navbar-fixed-top" role="navigation">
+    <div class="container">
+
+      <div class="navbar-header">
+        <button class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+          <span class="icon icon-bar"></span>
+          <span class="icon icon-bar"></span>
+          <span class="icon icon-bar"></span>
+        </button>
+        <a href="#top" class="navbar-brand smoothScroll">Barokah Rizki Motor</a>
+      </div>
+        <div class="collapse navbar-collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="/" class="smoothScroll"><span>Beranda</span></a></li>
+            <li><a href="/layanan" class="smoothScroll"><span>Layanan</span></a></li>
+            <li><a href="/gallery" class="smoothScroll"><span>Galeri</span></a></li>
+            <li><a href="/contact" class="smoothScroll"><span>Kontak</span></a></li>
+          </ul>
+       </div>
+
+    </div>
+  </div>
+
+
+<!-- Home section -->
+
+
+
+<!-- About section -->
+
+
+
+
+<!-- Gallery section -->
+
+<section id="gallery">
+  <div class="container">
+    <div class="row">
+
+      <div class="col-md-offset-2 col-md-8 col-sm-12 wow fadeInUp" data-wow-delay="0.3s">
+        <div class="section-title">
+          <h1>Galeri</h1>
+        </div>
+      </div>
+
+      <ul class="grid cs-style-4">
+        <li class="col-md-6 col-sm-6">
+          <figure>
+            <div><img src="{{asset('master/images/gallery-img1.jpg')}}" alt="image 2"></div>
+            <figcaption>
+              <p>Diupload Pada Tanggal</p>
+              <p>Senin, 21 November 2022</p>
+            </figcaption>
+          </figure>
+        </li>
+
+        <li class="col-md-6 col-sm-6">
+          <figure>
+            <div><img src="{{asset('master/images/gallery-img2.jpg')}}" alt="image 2"></div>
+            <figcaption>
+              <p>Diupload Pada Tanggal</p>
+              <p>Senin, 21 November 2022</p>
+            </figcaption>
+          </figure>
+        </li>
+
+        <li class="col-md-6 col-sm-6">
+          <figure>
+            <div><img src="{{asset('master/images/gallery-img3.jpg')}}" alt="image 2"></div>
+            <figcaption>
+              <p>Diupload Pada Tanggal</p>
+              <p>Senin, 21 November 2022</p>
+            </figcaption>
+          </figure>
+        </li>
+
+        <li class="col-md-6 col-sm-6">
+          <figure>
+            <div><img src="{{asset('master/images/gallery-img4.jpg')}}" alt="image 2"></div>
+            <figcaption>
+              <p>Diupload Pada Tanggal</p>
+              <p>Senin, 21 November 2022</p>
+            </figcaption>
+          </figure>
+        </li>
+      </ul>
+
+    </div>
+  </div>
+</section>
+
+
+<!-- Contact section -->
+
+
+
+
+<!-- Footer section -->
+
+
+
+<!-- Back top -->
+<a href="#" class="go-top"><i class="fa fa-angle-up"></i></a>
+
+<!-- Javascript  -->
+<script src="{{asset('master/js/jquery.js')}}"></script>
+<script src="{{asset('master/js/bootstrap.min.js')}}"></script>
+<script src="{{asset('master/js/vegas.min.js')}}"></script>
+<script src="{{asset('master/js/modernizr.custom.js')}}"></script>
+<script src="{{asset('master/js/toucheffects.js')}}"></script>
+<script src="{{asset('master/js/owl.carousel.min.js')}}"></script>
+<script src="{{asset('master/js/smoothscroll.js')}}"></script>
+<script src="{{asset('master/js/wow.min.js')}}"></script>
+<script src="{{asset('master/js/custom.js')}}"></script>
+<script src="{{asset('master/js/waypoints.min.js')}}"></script>
+
+</body>
+</html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,228 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="keywords" content="">
+	<meta name="description" content="">
+
+	<title>Barokah Rizki Motor</title>
+<!--
+Snapshot Template
+http://www.templatemo.com/tm-493-snapshot
+
+Zoom Slider
+https://vegas.jaysalvat.com/
+
+Caption Hover Effects
+http://tympanus.net/codrops/2013/06/18/caption-hover-effects/
+-->
+<link rel="stylesheet" href="{{asset('master/css/bootstrap.min.css')}}">
+	<link rel="stylesheet" href="{{asset('master/css/animate.min.css')}}">
+	<link rel="stylesheet" href="{{asset('master/css/font-awesome.min.css')}}">
+  	<link rel="stylesheet" href="{{asset('master/css/component.css')}}">
+	
+    <link rel="stylesheet" href="{{asset('master/css/owl.theme.css')}}">
+	<link rel="stylesheet" href="{{asset('master/css/owl.carousel.css')}}">
+	<link rel="stylesheet" href="{{asset('master/css/vegas.min.css')}}">
+	<link rel="stylesheet" href="{{asset('master/css/style.css')}}">
+
+
+	<!-- Google web font  -->
+	<link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700,300' rel='stylesheet' type='text/css'>
+	
+</head>
+<body id="top" data-spy="scroll" data-offset="50" data-target=".navbar-collapse">
+
+
+<!-- Preloader section -->
+
+<div class="preloader">
+     <div class="sk-spinner sk-spinner-pulse"></div>
+</div>
+
+
+<!-- Navigation section  -->
+
+  <div class="navbar navbar-default navbar-fixed-top" role="navigation">
+    <div class="container">
+
+      <div class="navbar-header">
+        <button class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+          <span class="icon icon-bar"></span>
+          <span class="icon icon-bar"></span>
+          <span class="icon icon-bar"></span>
+        </button>
+        <a href="#top" class="navbar-brand smoothScroll">Barokah Rizki Motor</a>
+      </div>
+        <div class="collapse navbar-collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="/" class="smoothScroll"><span>Beranda</span></a></li>
+            <li><a href="/layanan" class="smoothScroll"><span>Layanan</span></a></li>
+            <li><a href="/gallery" class="smoothScroll"><span>Galeri</span></a></li>
+            <li><a href="/contact" class="smoothScroll"><span>Kontak</span></a></li>
+          </ul>
+       </div>
+
+    </div>
+  </div>
+
+
+<!-- Home section -->
+
+<section id="home">
+  <div class="overlay"></div>
+  <div class="container">
+    <div class="row">
+
+      <div class="col-md-offset-1 col-md-10 col-sm-12 wow fadeInUp" data-wow-delay="0.3s">
+        <h1 class="wow fadeInUp" data-wow-delay="0.6s"></h1>
+        <p class="wow fadeInUp" data-wow-delay="0.9s"></p>
+        <a href="#about" data-wow-delay="1.2s"></a>
+      </div>
+
+    </div>
+  </div>
+</section>
+
+
+<!-- About section -->
+
+
+
+
+<!-- Gallery section -->
+
+<section id="gallery">
+  <div class="container">
+    <div class="row">
+
+      <div class="col-md-offset-2 col-md-8 col-sm-12 wow fadeInUp" data-wow-delay="0.3s">
+        <div class="section-title">
+          <h1>Galeri</h1>
+        </div>
+      </div>
+
+      <ul class="grid cs-style-4">
+        <li class="col-md-6 col-sm-6">
+          <figure>
+            <div><img src="{{asset('master/images/gallery-img1.jpg')}}" alt="image 2"></div>
+            <figcaption>
+              <p>Diupload Pada Tanggal</p>
+              <p>Senin, 21 November 2022</p>
+            </figcaption>
+          </figure>
+        </li>
+
+        <li class="col-md-6 col-sm-6">
+          <figure>
+            <div><img src="{{asset('master/images/gallery-img2.jpg')}}" alt="image 2"></div>
+            <figcaption>
+              <p>Diupload Pada Tanggal</p>
+              <p>Senin, 21 November 2022</p>
+            </figcaption>
+          </figure>
+        </li>
+
+        <li class="col-md-6 col-sm-6">
+          <figure>
+            <div><img src="{{asset('master/images/gallery-img3.jpg')}}" alt="image 2"></div>
+            <figcaption>
+              <p>Diupload Pada Tanggal</p>
+              <p>Senin, 21 November 2022</p>
+            </figcaption>
+          </figure>
+        </li>
+
+        <li class="col-md-6 col-sm-6">
+          <figure>
+            <div><img src="{{asset('master/images/gallery-img4.jpg')}}" alt="image 2"></div>
+            <figcaption>
+              <p>Diupload Pada Tanggal</p>
+              <p>Senin, 21 November 2022</p>
+            </figcaption>
+          </figure>
+        </li>
+      </ul>
+
+    </div>
+  </div>
+</section>
+
+
+<!-- Contact section -->
+
+<section id="contact">
+  <div class="container-fluid">
+    <div class="row">
+
+       <div class="col-lg-6 col-md-6 col-sm-12">
+
+        <div class="col-lg-offset-1 col-lg-8 section-title wow fadeInUp" data-wow-delay="0.4s">
+          <h1>Kontak Resmi</h1>
+          <p>Barokah Rizki Motor </p>
+          <p>Rawapasung, Sidanegara, Kec. Cilacap Tengah, Kabupaten Cilacap, Jawa Tengah 53224 </p>
+          <p>Telepon : 085742885109 </p>
+        </div>
+      </div>
+      <div class="col-lg-6 col-md-6 col-sm-12">
+        <div id="map">
+          <!-- How to change your own map point
+               1. Go to Google Maps
+               2. Click on your location point
+               3. Click "Share" and choose "Embed map" tab
+               4. Copy only URL and paste it within the src="" field below
+        -->
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3953.8380942419767!2d109.01925001432541!3d-7.700517778399825!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e65139c56d8beef%3A0xa4c91eaae06979e3!2sBRM%20Barokah%20Rizki%20Motor!5e0!3m2!1sen!2sid!4v1668822813858!5m2!1sen!2sid" width="100%" height="500px" frameborder="0" style="border:0" allowfullscreen></iframe>
+        </div>
+    </div>
+
+    </div>
+  </div>
+</section>
+
+
+<!-- Footer section -->
+
+<footer>
+	<div class="container">
+    
+		<div class="row">
+
+			<div class="col-md-12 col-sm-12">
+            
+                <ul class="social-icon"> 
+                    <li><a href="#" class="fa fa-facebook wow fadeInUp" data-wow-delay="0.2s"></a></li>
+                    <li><a href="#" class="fa fa-twitter wow fadeInUp" data-wow-delay="0.4s"></a></li>
+                    <li><a href="#" class="fa fa-linkedin wow fadeInUp" data-wow-delay="0.6s"></a></li>
+                    <li><a href="#" class="fa fa-instagram wow fadeInUp" data-wow-delay="0.8s"></a></li>
+                    <li><a href="#" class="fa fa-google-plus wow fadeInUp" data-wow-delay="1.0s"></a></li>
+                </ul>
+
+                
+			</div>
+			
+		</div>
+        
+	</div>
+</footer>
+
+<!-- Back top -->
+<a href="#" class="go-top"><i class="fa fa-angle-up"></i></a>
+
+<!-- Javascript  -->
+<script src="{{asset('master/js/jquery.js')}}"></script>
+<script src="{{asset('master/js/bootstrap.min.js')}}"></script>
+<script src="{{asset('master/js/vegas.min.js')}}"></script>
+<script src="{{asset('master/js/modernizr.custom.js')}}"></script>
+<script src="{{asset('master/js/toucheffects.js')}}"></script>
+<script src="{{asset('master/js/owl.carousel.min.js')}}"></script>
+<script src="{{asset('master/js/smoothscroll.js')}}"></script>
+<script src="{{asset('master/js/wow.min.js')}}"></script>
+<script src="{{asset('master/js/custom.js')}}"></script>
+<script src="{{asset('master/js/waypoints.min.js')}}"></script>
+
+</body>
+</html>
